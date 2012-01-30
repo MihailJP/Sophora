91d5ba19c0f2ee380a632b3ff41e2bd49ee1d0ae
TANIGAWA Takashi <mihailjp@gmail.com>
2012-01-30 14:51:26 +0900

    サブテーブル分割

--- utils/sh/scale.sh
+++ utils/sh/scale.sh
@@ -20,6 +20,7 @@ fontforge -lang=ff -c "Open(\"$1\");SelectWorthOutputting();Scale(130,0,0);Save(
 chkerr $?
 cat ${target}.tmp \
 	| sed -e "s/Position2: \"\[MONO\] Diacritics width adjustment-1\" dx=0 dy=0 dh=-500 dv=0/Position2: \"\[MONO\] Diacritics width adjustment-1\" dx=0 dy=0 dh=-650 dv=0/" \
+	| sed -e "s/Position2: \"\[MONO\] Diacritics width adjustment-1a\" dx=0 dy=0 dh=-500 dv=0/Position2: \"\[MONO\] Diacritics width adjustment-1a\" dx=0 dy=0 dh=-650 dv=0/" \
 	| sed -e "s/Position2: \"\[MONO\] Diacritics width adjustment-2\" dx=-500 dy=0 dh=-500 dv=0/Position2: \"\[MONO\] Diacritics width adjustment-2\" dx=-650 dy=0 dh=-650 dv=0/" \
 	> $2
 chkerr $?
